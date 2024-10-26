a36392a8d0f3607fa6499306d11568d62c93692a
zachey <63107653+zachey01@users.noreply.github.com>
2024-10-26 18:02:35 +0500

    Update @vercel/analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,8 @@
 import Home from "./components/sections/Home";
-import { Analytics } from "@vercel/analytics/react";
 
 const Main = () => {
   return (
     <div className="overflow-hidden">
-      <Analytics />
       <Home />
     </div>
   );
